9e68fa0749f517e6eba8facd5af30240baad2795
Sammy Park <saemipark7391@gmail.com>
2023-08-10 00:23:21 -0400

    day01

---
+++ JavaStudy/src/Study01/DataType.java
@@ -0,0 +1,10 @@
+package Study01;
+
+public class DataType {
+    public static void main(String[] args) {
+        System.out.println("String");
+        System.out.println(78134); //number
+        System.out.println(-3124);
+        System.out.println(7.35234);
+    }
+}
---
+++ JavaStudy/src/Study01/HelloWorld.java
@@ -0,0 +1,7 @@
+package Study01;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
