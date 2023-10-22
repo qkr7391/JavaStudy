63a4be61408725c2181432d1116b6221e25ec0cb
Saemi Park <saemipark7391@gmail.com>
2023-10-22 04:21:48 -0400

    Study03 - comment

---
+++ JavaStudy/src/Study03/comment.java
@@ -0,0 +1,13 @@
+package Study03;
+
+public class comment {
+    public static void main(String[] args) {
+        System.out.println("(10min before) Wedding will be start soon");
+//        System.out.println("(5min before) Wedding will be start soon"); // - command D
+        System.out.println("Wedding is start now");
+
+    int size = 120;
+    size = size+10; //130
+        System.out.println("size is " + size);
+    }
+}
