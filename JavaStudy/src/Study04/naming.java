a43e1a07fc5bff1c5e9b205a590872f28f34f940
Saemi Park <saemipark7391@gmail.com>
2023-10-23 16:41:36 -0400

    Study04 - naming

---
+++ JavaStudy/src/Study04/naming.java
@@ -0,0 +1,28 @@
+package Study04;
+
+public class naming {
+    public static void main(String[] args) {
+        //meaningful
+        //_, char, num >> available
+        //space >> not available
+        // not start with num
+        //low case start or carmel
+
+        //ex
+        String nationality = "Canada";
+        String firstName = "Leo";
+        String lastName = "Park";
+        String birthDate = "2024-12-31";
+        String address = "Toronto";
+
+        String _start="start";
+        String middle_underbar="middle";
+        String mix_with_7 = "12345";
+
+        int size = 7;
+        int _rate= 10;
+        int time_assume = 9;
+        int min_time_8 = 100;
+
+    }
+}
