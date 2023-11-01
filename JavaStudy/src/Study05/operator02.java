f61553d23141a80b70af4d890a7220ecfec56d58
Saemi Park <saemipqrk7391@gmail.com>
2023-11-01 16:42:50 -0400

    Study05 - operator02

--- JavaStudy/src/Study05/operator02.java
+++ JavaStudy/src/Study05/operator02.java
@@ -6,5 +6,36 @@ public class operator02 {
         num = num + 2;
         System.out.println(num); //12
 
+        num = num - 2;
+        System.out.println(num); //10
+
+        num = num * 2;
+        System.out.println(num); //20
+
+        num = num / 2;
+        System.out.println(num); //10
+
+        num = num % 2;
+        System.out.println(num); // 0
+
+        num = 10;
+        num += 2;
+        System.out.println(num); //12
+
+        num -= 2;
+        System.out.println(num); //10
+
+        num *= 2;
+        System.out.println(num); //20
+
+        num /= 2;
+        System.out.println(num); //10
+
+        num %= 2;
+        System.out.println(num); //0
+
+
+
+
     }
 }
