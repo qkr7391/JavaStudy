f5b5e09bb3828034d6d399b1e4fd22b1d0c0e080
Saemi Park <saemipqrk7391@gmail.com>
2023-11-01 16:37:18 -0400

    Study05 - operator01

---
+++ JavaStudy/src/Study05/operator02.java
@@ -0,0 +1,10 @@
+package Study05;
+
+public class operator02 {
+    public static void main(String[] args) {
+        int num = 10;
+        num = num + 2;
+        System.out.println(num); //12
+
+    }
+}
