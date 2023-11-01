2b32441bd252da7478d2bdcfa38797a0bd4829ca
Saemi Park <saemipqrk7391@gmail.com>
2023-11-01 17:08:10 -0400

    Study05 - operator05

---
+++ JavaStudy/src/Study05/quiz.java
@@ -0,0 +1,15 @@
+package Study05;
+
+public class quiz {
+    public static void main(String[] args) {
+     int childA = 115;
+     int childB = 121;
+
+     String s1 = (childA >= 120) ? "Yes" : "No";
+        System.out.println(s1); //No
+
+
+        String s2 = (childB >= 120) ? "Yes" : "No";
+        System.out.println(s2); //Yes
+    }
+}
