acd9942c3c785b1550a83bd6ad4724e0855b4915
Saemi Park <saemipqrk7391@gmail.com>
2023-11-01 16:48:14 -0400

    Study05 - operator02

---
+++ JavaStudy/src/Study05/operator03.java
@@ -0,0 +1,19 @@
+package Study05;
+
+public class operator03 {
+    public static void main(String[] args) {
+        System.out.println(5 > 3); //true
+        System.out.println(5 >= 3); //true
+        System.out.println(5 >= 5); //true
+        System.out.println(5 >= 7); //false
+
+        System.out.println(5 < 3); //false
+        System.out.println(5 <= 3); //false
+
+        System.out.println(5 == 5); //true
+        System.out.println(5 == 3); //false
+        System.out.println(5 != 5); //false
+        System.out.println(5 != 3); //true
+
+    }
+}
