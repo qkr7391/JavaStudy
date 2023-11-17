863c635cca1d9c41460a45514a324d46a83f4a78
Saemi Park <saemipqrk7391@gmail.com>
2023-11-17 00:12:49 -0500

    Study09 - switch case

---
+++ JavaStudy/src/Study09/forStatement.java
@@ -0,0 +1,23 @@
+package Study09;
+
+public class forStatement {
+    public static void main(String[] args) {
+        //for statement
+
+        for (int i = 0 ; i < 10; i++){
+            System.out.println(i);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0 && i != 0){
+                System.out.println(i);
+            }
+        }
+
+        int sum = 0;
+        for (int i = 1 ; i < 11; i++){
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
