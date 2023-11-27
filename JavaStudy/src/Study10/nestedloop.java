49455de76ababdef8dc07e03d4b7a7bc55ea5ebb
Saemi Park <saemipqrk7391@gmail.com>
2023-11-26 21:44:59 -0500

    Study10 - Nested Loop(print, println)

---
+++ JavaStudy/src/Study10/nestedloop.java
@@ -0,0 +1,13 @@
+package Study10;
+
+public class nestedloop {
+    public static void main(String[] args) {
+        //making * shape
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
