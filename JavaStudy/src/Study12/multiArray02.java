5f8b3da7a8b845194acfdb9d0f0778841b79ad4c
Saemi Park <saemipqrk7391@gmail.com>
2023-12-11 17:27:51 -0500

    Study12 - Multidimensional arrays 02

---
+++ JavaStudy/src/Study12/multiArray02.java
@@ -0,0 +1,16 @@
+package Study12;
+
+public class multiArray02 {
+    public static void main(String[] args) {
+        String[][] seats = {
+                {"a1", "a2", "a3"},
+                {"b1", "b2", "b3", "b4"},
+                {"c1", "c2", "c3", "c4", "c5"}
+        };
+
+        //a3
+        System.out.println(seats[0][2]);
+        //b4
+        System.out.println(seats[1][3]);
+    }
+}
