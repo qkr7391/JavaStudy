f777ad7fdaa63153e864b07707efcae6629d01d3
Saemi Park <saemipqrk7391@gmail.com>
2023-12-14 19:09:19 -0500

    Study13 - parameter and return

---
+++ JavaStudy/src/Study13/paramReturn.java
@@ -0,0 +1,11 @@
+package Study13;
+
+public class paramReturn {
+
+    public static int getPower(int num){
+        return (num * num);
+    }
+    public static void main(String[] args) {
+        System.out.println(getPower(2));
+    }
+}
